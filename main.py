14b11452077758200917403bcc27b0d1c985f3bd
Shristi Sehwag <91873414+ShristiSehwag@users.noreply.github.com>
2024-07-15 16:11:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def get_news(topic, from_date, to_date, language='en',
+            api_key='17fb352c8f7949a6922ed1c7833fb8aa'): 
+  url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}8&sortBy=popularity&language={language}&apiKey={api_key}'
+  r = requests.get(url)
+  content = r.json()
+  articles = content['articles']
+  results = []
+  for article in articles:
+    print(f"\nTITLE\n{article['title']} \nDESCRIPTION:\n {article['description']}")
+  
+
+# NOTE: Change the from_date and to_date below to reflect recent dates
+# Otherwise, you will get an error.
+get_news(topic='space', from_date='2024-6-15', to_date='2024-6-15')
