3c2e30662738e33dc0f1bf46f43d4e3b4549fc0b
Shristi Sehwag <91873414+ShristiSehwag@users.noreply.github.com>
2024-07-16 09:06:47 +0000

    extract images from fb account

--- facebook_data_api.py
+++ facebook_data_api.py
@@ -1,8 +1,16 @@
 import requests
 
-url = '' # u'r cURL Value from fb developer account
+url = " " # u'r cURL Value from fb developer account
 def get_fb_response():
   response = requests.get(url)
   print(response.text)
+
+  # to fetch an image of account that you have access to 
+  data = json.loads(data)
+  image_url = data['images'][0]['source']
+  image_bytes = requests.get(image_url).content
+
+  with open('image.jpg', 'wb') as file:
+    file.write(image_bytes)
 
 
