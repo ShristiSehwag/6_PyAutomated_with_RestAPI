85bdd963a17918151995ddeb4dae4fa8153491cf
Shristi Sehwag <91873414+ShristiSehwag@users.noreply.github.com>
2024-07-16 05:41:52 +0000

    creating own currency API

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import weather_forecast as w
 import get_news as n
+import currency_api_own as c
 
 # n.get_news(topic='space', from_date='2024-6-16', to_date='2024-6-16')
 
-w.get_weather(city='Washington')
+# w.get_weather(city='Washington')
+
+c.app.run(host='0.0.0.0')
